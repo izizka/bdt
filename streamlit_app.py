86ae01d9dad7e15cf8ae265d2a4bc5e684d82865
Ivan Zizka <zizkaiva@fel.cvut.cz>
2022-12-26 22:32:52 +0100

    complete version not final

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ So I changed the definition of uniques - I counted number of unique train's trip
 """)
 st.subheader("Select Date")
 date_selected = st.date_input(
-    " ",
+    "Number of vehicles selector",
     value=datetime.date(2022, 12, 20),
     min_value=datetime.date(2022, 12, 18),
     max_value=datetime.date(2022, 12, 23))
@@ -78,6 +78,83 @@ st.write("""This task is more complex then previous one. We have two types of ap
 st.write("""First one is to use information sent by vehicle about current speed. There are two problems with this approach. First - data are not send in same intervals. 
 Second - Trains, Trams do not send this type of information (always null) on the other hand boats send this type of information but is often zero (waiting time on the bank of river).
 Second issue can be avoid by computing the average speed on given line within a hour. The average speed for all vehicles within hour is just 
-average of average line speed. """)
-st.write("""Second attitude is easier and more stable/consistent.""")
+average of average line speed. This way of solving count with the randomness in period when the message is sent. Lets say that there is a unify
+random distribution of place where the msg is sent. Soo it means that the probability of sanding message in a stop is same as probability of sending message in part of the road between stops.""")
+st.write("""Second attitude is easier and more stable/consistent. The main idea is to sum traveled distances for each car within hour and compute average distance traveled.
+The time window is equal to one hour soo average traveled distance is also average speed for vehicles. I can use similar attitude as in the first approach where current speed is missing.
+Instead of computing average speed I would use just distance and time would be one hour.""")
 
+st.write("""In the following section you can find average speed computed with both approaches (first attitude = first figure | second attitude = second figure)""")
+
+
+st.subheader("Select Date")
+date_selected_avg = st.date_input(
+    "AVG speed selector",
+    value=datetime.date(2022, 12, 20),
+    min_value=datetime.date(2022, 12, 18),
+    max_value=datetime.date(2022, 12, 23))
+date_selected_avg = date_selected_avg.day
+
+st.subheader("Buses")
+df_buses = pd.read_csv("csvs/avg_random_buses.csv", header=0)
+filter_df = df_buses.loc[df_buses["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+
+df_buses = pd.read_csv("csvs/avg_buses.csv", header=0)
+filter_df = df_buses.loc[df_buses["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+
+
+st.subheader("RegBuses")
+df_regbuses = pd.read_csv("csvs/avg_random_regbuses.csv", header=0)
+filter_df = df_regbuses.loc[df_regbuses["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+
+df_regbuses = pd.read_csv("csvs/avg_regbuses.csv", header=0)
+filter_df = df_regbuses.loc[df_regbuses["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+
+st.subheader("Trams")
+df_trams = pd.read_csv("csvs/avg_random_trams.csv", header=0)
+filter_df = df_trams.loc[df_trams["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+
+df_trams = pd.read_csv("csvs/avg_trams.csv", header=0)
+filter_df = df_trams.loc[df_trams["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+
+st.subheader("Boats")
+df_boats = pd.read_csv("csvs/avg_random_boats.csv", header=0)
+filter_df = df_boats.loc[df_boats["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+
+df_boats = pd.read_csv("csvs/avg_boats.csv", header=0)
+filter_df = df_boats.loc[df_boats["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+st.write("The main problem with boats is that there is not enough data. Also there is not huge different between approaches.")
+
+st.subheader("Trains")
+
+df_trains = pd.read_csv("csvs/avg_random_trains.csv", header=0)
+filter_df = df_trains.loc[df_trains["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+
+df_trains = pd.read_csv("csvs/avg_trains.csv", header=0)
+filter_df = df_trains.loc[df_trains["day_number"] == date_selected_avg]
+filter_df = filter_df[["hour_number", "avg_speed"]]
+st.bar_chart(filter_df, x='hour_number', y='avg_speed')
+st.write("""Here is the biggest difference between approaches. I double checked the results and I did no find any mistake.
+    I would say that the second figure is more accurate. The first attitude does not work in this case because of the speed between stops. 
+    Train is really fast if it is not in a stop. The speed limit is around 160 km/h in some cases 120 km/h. We can assume that the speed limit 
+    in Prague's lines is lower. On the other hands, it is still very high compare to other vehicles. I believe that it is the reason behind the
+    difference. 
+""")
